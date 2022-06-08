99df993a1d1d780e813e7ed7d1434acc6302ecaa
kibbumJoy <107078220+kibbumJoy@users.noreply.github.com>
2022-06-08 13:59:07 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 // complete the function
 function prime (num) {
   // code goes here
-  if(num % 1 == 0) {
-    return true
-  }
-  else{
-    return false
-  }
+  return num <= 1 ? false : num % 2 == 0 && num > 2 ? false : num == 2 ? true : num % 1 == 0 ? true : false
 }
 
 function solution (arg) {
